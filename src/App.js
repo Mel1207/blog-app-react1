ac1f43e33603e8e19100ebc27f95f8aa75596c6a
Mel <meljohnpualon@gmail.com>
2021-05-11 06:10:00 +0800

    added react dynamic valued elements for example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+  const title = 'Elcome to the new blog'
+  const likes = 50;
+  const link = "https://www.google.com/"
+
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{ 10 }</p>
+        <p>{ "Hey mel"}</p>
+        <p>{ [1, 2, 3, 4, 5] }</p>
+        <p>{ Math.random() * 20 }</p>
+
+        <a href={ link } target="blank">Google site</a>
       </div>
     </div>
   );
