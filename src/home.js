fdec5caa348f57db50c7cb9759e53f50930b6c8d
Mel <meljohnpualon@gmail.com>
2021-05-12 06:40:40 +0800

    added click events

--- src/home.js
+++ src/home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('hey there!')
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hey '+ name);
+    }
+
     return ( 
         <div className="home">
             <h2>this is homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => {
+                handleClickAgain('Mel')
+            }}>Click Me Again</button>
         </div>
      );
 }
