1ea9128829d0ab7b69c4b35516bf4598a66242bf
Mel <meljohnpualon@gmail.com>
2021-05-13 20:14:43 +0800

    added router links

--- src/navbar.js
+++ src/navbar.js
@@ -1,15 +1,17 @@
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>My blog</h1>
             <div className="links">
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
                 {/* <a href="/" style={{
                     color: "white",
                     backgroundColor: "#f1356d",
                     borderRadius: "8px"
                 }}>New Blog</a> */}
-                <a href="/">New Blog</a>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
     );
