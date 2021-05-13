7b80c000743c4dc2a226a649b0dafe758833f608
Mel <meljohnpualon@gmail.com>
2021-05-13 09:21:04 +0800

    used filter to separate blogs of same authors

--- src/home.js
+++ src/home.js
@@ -6,12 +6,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Mel', id: 1},
         { title: 'The black parade', body: 'lorem ipsum...', author: 'John', id: 2},
-        { title: 'CSS Life', body: 'lorem ipsum...', author: 'Joe', id: 3},
+        { title: 'CSS Life', body: 'lorem ipsum...', author: 'Mel', id: 3},
     ]);
     
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Mel')} title="Mel Blogs"/>
         </div>
     );
 }
