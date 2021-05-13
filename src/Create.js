6dc51d5088daac8636d1448dbd5bac289ea8b18f
Mel <meljohnpualon@gmail.com>
2021-05-13 23:14:55 +0800

    added post request to json server

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,23 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mel')
+    const [isPending, setIsPending] = useState(false);
     
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = {title, body, author}
-        console.log(blog)
+
+        setIsPending(true);
+        
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        })
+        .then(() => {
+            console.log('new blog added')
+            setIsPending(false)
+        })
     } 
 
     return ( 
@@ -36,7 +48,8 @@ const Create = () => {
                     <option value="mel">Mel</option>
                     <option value="john">John</option>
                 </select>
-                <button>Add Blog</button>
+                { !isPending && <button>Add Blog</button> }
+                { isPending && <button>Adding new blog ...</button> }
                 <p>{title}</p>
                 <p>{body}</p>
                 <p>{author}</p>
